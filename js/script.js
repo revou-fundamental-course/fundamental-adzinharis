d5e8da4dd3edc06d659e09d20be40bcc4bd55b94
Adzin Abdul Haris <adzinharis34@gmail.com>
2024-07-04 11:24:00 +0700

    final

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ window.onload = function() {
     }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('messageForm').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const nama = document.querySelector('input[name="nama"]').value;
+        const tanggal = document.querySelector('input[name="tanggal"]').value;
+        const gender = document.querySelector('input[name="gender"]:checked').value;
+        const pesan = document.querySelector('input[name="pesan"]').value;
+
+        document.getElementById('displayNama').textContent = nama;
+        document.getElementById('displayTanggal').textContent = tanggal;
+        document.getElementById('displayGender').textContent = gender;
+        document.getElementById('displayPesan').textContent = pesan;
+    });
+});
